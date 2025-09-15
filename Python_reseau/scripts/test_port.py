4f6c1e0da2aa3866fa062c348803d6992dfd7399
Noah <noah.delahousse@gmail.com>
2025-09-15 18:17:44 +0200

    feat(scripts) : amélioration du script permettant de scanner plusieurs ports

--- Python_reseau/scripts/test_port.py
+++ Python_reseau/scripts/test_port.py
@@ -1,18 +1,19 @@
 import sys
 import socket
 
-if len(sys.argv)==3:
+if len(sys.argv)>3:
     ip=sys.argv[1]
-    port=int(sys.argv[2])
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        s.connect((ip,port))
-        print("Connexion réussie, le port est ouvert")
-    except ConnectionRefusedError:
-        print("Port fermé (connexion refusée)")
-    except Exception as e:
-        print(f"Erreur inattendue : {e}")
-    finally:
-         s.close()
+    for i in range (2,len(sys.argv)):
+        port=int(sys.argv[i])
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            s.connect((ip,port))
+            print("Connexion réussie, le port est ouvert")
+        except ConnectionRefusedError:
+            print(f"Port {port} fermé (connexion refusée)")
+        except Exception as e:
+            print(f"Erreur inattendue : {e}")
+        finally:
+            s.close()
 else:
-    print("Usage : python3 test_port.py ip (ou localhost) port")
+    print("Usage : python3 test_port.py ip (ou localhost) port1 port2 ...")
