53d3036cabf33e9e6de22ddbcec21d3d67e07ba3
Noah <noah.delahousse@gmail.com>
2025-09-15 18:53:42 +0200

    feat(scripts) : amélioration du script en utilisant une fonction

--- Python_reseau/scripts/test_port.py
+++ Python_reseau/scripts/test_port.py
@@ -1,19 +1,23 @@
 import sys
 import socket
+def scan_port(ip,port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        s.connect((ip,port))
+        message="connexion réussie, le port est ouvert"
+    except ConnectionRefusedError:
+        message="port fermé (connexion refusée)"
+    except Exception :
+        message="erreur inattendue"
+    finally:
+            s.close()
+            return message
 
 if len(sys.argv)>3:
     ip=sys.argv[1]
     for i in range (2,len(sys.argv)):
         port=int(sys.argv[i])
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        try:
-            s.connect((ip,port))
-            print("Connexion réussie, le port est ouvert")
-        except ConnectionRefusedError:
-            print(f"Port {port} fermé (connexion refusée)")
-        except Exception as e:
-            print(f"Erreur inattendue : {e}")
-        finally:
-            s.close()
+        message=scan_port(ip,port)
+        print(f"Port:{port}",message)
 else:
     print("Usage : python3 test_port.py ip (ou localhost) port1 port2 ...")
